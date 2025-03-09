543f5eb60b9d4730f4526d5f4838a88f028c2df9
Nicolas Daniel Klaue <83474612+NicolasKlaue@users.noreply.github.com>
2025-03-09 10:34:18 +0000

    Current Ver

--- reactfolio/src/App.js
+++ reactfolio/src/App.js
@@ -24,6 +24,7 @@ function App() {
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Homepage />} />
+				<Route path="/Portfolio" element={<Homepage />} />
 				<Route path="/Portfolio/" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
